f3b7cac9fb3da69b671b854d080f8bb1405bf21e
Justin Tsang <justin.b.tsang@gmail.com>
2016-10-28 01:25:05 +0000

    changed some routes and put them inside the app file. some comment functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,12 @@ $(() => {
   //   }
   // });
 
+  $('button').on("click" ,function(event){
+    $('textarea').slideToggle();
+    $('textarea').trigger("focus");
+    return false;
+  });
+
   $.get('/api/users').then(users => {
     const dom = users.map(user => {
       // return $("<div/>").html(user.username);
@@ -15,35 +21,52 @@ $(() => {
 
     });
 
-    $('body').append(dom);
+    //$('body').append(dom);
   });
-  $.get('/api/comments').then(comments => {
-    const dom = comments.map(user => {
-      // return $("<div/>").html(user.username);
-      return $("<p>" + user.comment + "</p>");
 
-    });
-
-    $('p').append(dom);
-  });
+  // <section class="resource-container">
+  //   <article>
+  //     <a href="http://google.ca">http://google.ca</a>
+  //     <ul>
+  //       <li>Comment 1</li>
+  //       <li>Comment 2</li>
+  //     </ul>
+  //   </article>
+  // </section>
 
   $.get('/api/resources').then(resources => {
-    const dom = resources.map(user => {
-      // return $("<div/>").html(user.username);
-      return $("<a>" + user.urls + ">");
+    let containerTag = $('<section>').addClass('resource-container');
+
+    const dom = resources.forEach(resource => {
+        let resourceTag = $("<article>");
+
+        resourceTag.append($('<a href="' + resource.urls + '">' + resource.urls + '</a>'));
+        let commentTag = resourceTag.append($('<ul>'));
+
+        $.get('/api/resources/' + resource.resourcesid + '/comments').then(comments => {
+          const dom = comments.forEach(comment => {
+            // return $("<div/>").html(user.username);
+            commentTag.append($("<li>" + comment.comment + "</li>"));
+
+          });
+        });
+
+      containerTag.prepend(resourceTag);
 
     });
 
-    $('img').append(dom);
+    $('body').append(containerTag);
+
+    //$('img').append(dom);
   });
-  $.get('/api/user_activity').then(user_activity => {
-    const dom = user_activity.map(user => {
+  $.get('/api/activity').then(activity => {
+    const dom = activity.map(user => {
       // return $("<div/>").html(user.username);
       // return $("<p>" + user.likes + "</p>");
       return $("<p>" + user.ratings + "</p>");
 
     });
 
-    $('p .rating').append(dom);
+    //$('p .rating').append(dom);
   });
 });
